371b6a8b72c53100047152ef5bc1227ffb2a005d
Keith Hemsoth <kevhemsoth@gmail.com>
2020-04-10 10:15:45 -0400

    functionality: home page image link to proper url

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,19 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
     <h2>Choose a Simulator:</h2>
     <div class="sim-list">
-      <img src="../assets/assetto-corsa-logo.jpg" class="logo img-fluid" alt="Responsive image">
-      <img src="../assets/acc-logo.webp" class="logo img-fluid" alt="Responsive image">
-      <img src="../assets/iracing-logo.jpg" class="logo img-fluid" alt="Responsive image">
-      <img src="../assets/raceroom-logo.png" class="logo img-fluid" alt="Responsive image">
+      <router-link to="/assetto-corsa">
+        <img src="../assets/assetto-corsa-logo.jpg" class="logo img-fluid" alt="AC logo">
+      </router-link>
+      <router-link to="/acc">
+        <img src="../assets/acc-logo.webp" class="logo img-fluid" alt="ACC logo">
+      </router-link>
+      <router-link to="/iracing">
+        <img src="../assets/iracing-logo.jpg" class="logo img-fluid" alt="iRacing logo">
+      </router-link>
+      <router-link to="/raceroom">  
+        <img src="../assets/raceroom-logo.png" class="logo img-fluid" alt="RaceRoom logo">
+      </router-link>  
     </div>
   </div>
 </template>
