5d0f45f230323e5af21a065811b42d853ef6bdb8
Keith Hemsoth <kevhemsoth@gmail.com>
2020-04-12 12:14:13 -0400

    Functionality: Added Tracks component to all sims

--- src/views/RaceRoom.vue
+++ src/views/RaceRoom.vue
@@ -1,11 +1,18 @@
 <template>
   <div>
     <p>This will be the R3E landing page</p>
+
+    <Tracks />
   </div>  
 </template>
 
 <script>
+import Tracks from '../components/Tracks';
+
 export default {
+    components: {
+      Tracks
+  }
 
 }
 </script>
--- src/views/iRacing.vue
+++ src/views/iRacing.vue
@@ -1,11 +1,18 @@
 <template>
   <div>
     <p>This will be the iRacing landing page</p>
+
+    <Tracks />
   </div>  
 </template>
 
 <script>
+import Tracks from '../components/Tracks';
+
 export default {
+    components: {
+      Tracks
+  }
 
 }
 </script>
